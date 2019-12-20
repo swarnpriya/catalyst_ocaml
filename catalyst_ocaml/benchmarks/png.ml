305d4326f01338efeea980c4121cd6ce5e82fd87
priyas <priyas@purdue.edu>
2019-12-21 01:06:19 +0530

    some more changes

--- catalyst_ocaml/benchmarks/png.ml
+++ catalyst_ocaml/benchmarks/png.ml
@@ -21,7 +21,9 @@ match l1 with
                 let temp2 = x1::temp1 in 
                 temp2
 
-
+(* Specification:
+   (1) Last bit should be zero 
+   (2) Rest of the bits should be same as input *)
 let shift_left_one_bit l2 = match l2 with 
 | [] -> []
 | x :: xs -> concat xs [0]
